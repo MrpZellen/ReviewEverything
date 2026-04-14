05e5c241d3f03ebd495db4cea81fc96476bcc2f0
MrpZellen <158220937+MrpZellen@users.noreply.github.com>
2026-04-14 00:53:03 -0600

    completed docker features.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // defines react as our primary framework
+  server: { //server config defines express connection for Vite.
+    host: '0.0.0.0',  // expose to Docker, removing internal network restrictions.
+    port: 5173,
+    proxy: {
+      '/api': 'http://localhost:3100'  // proxy means that we can define api routes and auto forward to express. 
+      // TODO: ensure every api instance starts with /api to forward to express servers and NOT register as a react page to work with Vite properly.
+    }
+  }
 })
