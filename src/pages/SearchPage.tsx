be5da552faf07639bb80d31565ee4f006b67fb7a
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-04-16 09:44:04 -0600

    Search for genres
    The moviedb doesn't actually have a way to search a genre but it sends the genre id to the api and returns it

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,25 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function SearchPage(){
     const [query, setQuery] = useState("");
     const [results, setResults] = useState<any[]>([]);
     const [searched, setSearched] = useState(false);
 
+    const [genres, setGenres] = useState<any[]>([]);
+    const [selectedGenre, setSelectedGenre] = useState("");
+
+
     const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
+    useEffect(() => {
+        const fetchGenres = async () => {
+            try{
+                const res = await fetch(
+                    `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
+                );
+                const data = await res.json();
+                setGenres(data.genres || []);
+            }catch(err){
+                console.error(err);
+            }
+        };
+        fetchGenres();
+    }, []);
+
     const handleSearch = async () => {
-        if(!query.trim()) return;
-        console.log("Searching for:", query);
         setSearched(true);
         try{
-            const res = await fetch(
-                `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`
-            );
+            let url = "";
+            if(selectedGenre){
+                url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${selectedGenre}`;
+            }
+            else if(query.trim()){
+                url =   `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`;
+            }
+            if(!url) return;
+            const res = await fetch(url);
             const data = await res.json();
             setResults(data.results || []);
-            setSearched(true);
-        }catch (err){
-            console.error(err);
+        }catch(err){
+            console.log(err);
             setResults([]);
         }
     };
@@ -32,6 +54,17 @@ export default function SearchPage(){
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
             />
+            <select
+                value={selectedGenre}
+                onChange={(e) => setSelectedGenre(e.target.value)}
+            >
+                <option value="">All Genres</option>
+                {genres.map((genre) => (
+                    <option key={genre.id} value={genre.id}>
+                        {genre.name}
+                    </option>
+                ))}
+            </select>
             <button onClick={handleSearch}>Search</button>
 
             <div>
