95fa74b4a2ef260981a9ccf502e12784e606ff0f
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-04-22 19:04:16 -0600

    For testing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './login.tsx'
+import SearchPage from './pages/SearchPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Login />
+    <SearchPage/>
+    {/* Temporary placement for testing */}
   </StrictMode>,
 )
