1e0f63a090659df8c12ac90a37c215396ae383c8
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-04-29 13:45:15 -0600

    HomePage/Navbar
    I added stylesheets and a retro neon theme to my pages. This is a temporary theme.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import "./navbar.css";
 
 export default function Navbar(){
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/search">Search</Link>
+        <nav className="navbar">
+            <div className="nav-logo">Review Everything</div>
+            <div className="nav-links">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/search" className="nav-link">Search</Link>
+            </div>
         </nav>
     );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
+import "./style/home.css";
 
 export default function HomePage(){
     return(
-        <div>
-            <h1>Review Everything</h1>
+        <div className="home-page">
+            <div className="home">
+                <h1 className="home-title">Review Everything</h1>
+                <p className="home-tag">Discover movies and review them! </p>
+            </div>
         </div>
     )
 }
