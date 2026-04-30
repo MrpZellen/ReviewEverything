e762b3626d0f64b0f68ab07184e3c8a61168f71c
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-04-30 09:32:50 -0600

    Created MovieDetailspage
    I have created a very basic version of the movie details page

---
+++ src/pages/MovieDetailsPage.tsx
@@ -0,0 +1,14 @@
+export default function MovieDetails() {
+    return (
+        <div>
+            <div className="backdrop" />
+            <div className="content">
+                <img />
+                <div className="details">
+                    <h1>title</h1>
+                    <p>overview</p>
+                </div>
+            </div>
+        </div>
+    )
+}
