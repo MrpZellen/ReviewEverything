aff7f78a577bf527eabd42245ac4dbbc61dd473e
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-04-30 14:25:38 -0600

    Made MovieDetails functional and pretty
    The movie details page can now display the details of the movie. It displays title, overview, poster, tagline, release date, and genres. At the end, I added a basic version a cast list.

--- src/pages/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage.tsx
@@ -1,12 +1,55 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom"
+import "./style/moviedetails.css";
+
 export default function MovieDetails() {
+    const { id } = useParams();
+    const [movie, setMovie] = useState<any>(null);
+    const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+    useEffect(() => {
+        const fetchMovie = async () => {
+            const res = await fetch(
+                `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`
+            );
+            const data = await res.json();
+            setMovie(data);
+        };
+        fetchMovie();
+    }, [id]);
+
+    if (!movie) return <p>Loading...</p>;
+
     return (
-        <div>
-            <div className="backdrop" />
+        <div className="movie-details">
+            <div className="backdrop" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path || movie.poster_path})`}}/>
             <div className="content">
-                <img />
-                <div className="details">
-                    <h1>title here</h1>
-                    <p>overview of the movie here</p>
+                <Link to="/search" className="back-button">←</Link>
+                <div className="details-main">
+                    <img className="movie-detail-poster" src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
+                    <div className="details">
+                        <h1 className="movie-detail-title">{movie.title}</h1>
+                        {movie.tagline && (
+                            <p className="movie-tagline">{movie.tagline}</p>
+                        )}
+                        <p className="movie-release">{movie.release_date}</p>
+                        <div className="movie-genres">
+                            {movie.genres?.map((g: any) => (
+                                <span key={g.id} className="genre-pill">
+                                    {g.name}
+                                </span>
+                            ))}
+                        </div>
+                        <p className="movie-overview">{movie.overview}</p>
+                    </div>
+                </div>
+                <div>
+                    <h2>Cast</h2>
+                    <div className="cast-section">
+                        <div>
+                            <p>actor name</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
