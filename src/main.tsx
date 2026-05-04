6504651cc229dce8a7ea4f3bc9476e661b14a96f
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-05-03 18:21:34 -0600

    rebase errors fixed

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './login.tsx'
 import SearchPage from './pages/SearchPage.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login />
-    <SearchPage/>
-    {/* <App/> */}
+    {/* <Login /> */}
+    {/* <SearchPage/> */}
+    <App/>
   </StrictMode>,
 )
