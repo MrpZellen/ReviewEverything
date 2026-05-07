d52656000e5c3e44a713a9642aa2105eaeb86660
Amanda Hezekiah <ahezekiah@student.neumont.edu>
2026-05-07 08:47:26 -0600

    Updated the Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-import SearchPage from './pages/SearchPage'
-import HomePage from './pages/HomePage';
-import Navbar from './components/Navbar';
-import MovieDetails from './pages/MovieDetailsPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import MovieDetails from "./pages/MovieDetailsPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar/>
+      <Navbar />
+
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/search" element={<SearchPage/>}/>
-        <Route path='/movie/:id' element={<MovieDetails/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
