db373e1173b52e406ad3df44f953e6acd6a179a8
Debrown-design <debrown@student.neumont.edu>
2026-05-07 08:53:04 -0600

    Register Added to Main Page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Login from './login.tsx'
+// import Login from './login.tsx'
+import Register from './frontend/register.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login />
+    {/* <Login /> */}
+    <Register />
   </StrictMode>,
 )
