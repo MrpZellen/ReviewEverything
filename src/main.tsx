0f3f4d57502a4da2a3355c2f44a1f0edcc8b6a3f
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2026-05-12 14:48:28 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Login from './login.tsx'
-import SearchPage from './pages/SearchPage.tsx'
+// import Login from './login.tsx'
+// import SearchPage from './pages/SearchPage.tsx'
 import App from './App.tsx'
 // import Login from './login.tsx'
-import Register from './frontend/register.tsx'
+// import Register from './frontend/register.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
