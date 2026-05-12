b031c54ca61d7ac999508e3bd3aa2d5f3dd94022
MrpZellen <158220937+MrpZellen@users.noreply.github.com>
2026-05-12 09:23:35 -0600

    adminining it

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -3,6 +3,18 @@ import "./style/adminpage.css";
 
 export default function AdminPage() {
   const [loading, setLoading] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
 
-  return (<main></main>);
+  return (
+  <main>
+    {(loading) && (
+        <p>loading...</p>
+    )}
+    {(isAdmin && !loading) && 
+    (<p>admin and loaded</p>)
+    }
+    {(!isAdmin && !loading) && 
+    (<p>not admin, send back</p>)}
+  </main>
+  );
 }
