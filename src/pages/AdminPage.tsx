575661d1e2d23e74fd9a4453a85ab6d8c65d7ea1
MrpZellen <158220937+MrpZellen@users.noreply.github.com>
2026-05-12 09:18:22 -0600

    base admin page

---
+++ src/pages/AdminPage.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import "./style/adminpage.css";
+
+export default function AdminPage() {
+  const [loading, setLoading] = useState(true);
+
+  return (<main></main>);
+}
