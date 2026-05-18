efa6cb8af0206296db741070a29ee012fe951cd0
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2026-05-18 09:39:31 -0600

    Write Review Form UI
    The UI for the review form is made. is connected to the API. Needs to be connected to the Database. Also tester is in the navbar but can be commented out.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,6 +56,16 @@ export default function Navbar() {
         >
           Reviews
         </NavLink>
+
+        <NavLink
+          to="/write-review/:movieID"
+          className={({ isActive }) =>
+            isActive ? "nav-link active" : "nav-link"
+          }
+        >
+          Write Review
+        </NavLink>
+        
       </div>
 
       <button className="signin-btn">Sign In</button>
