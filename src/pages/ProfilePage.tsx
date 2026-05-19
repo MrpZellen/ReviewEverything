4d8fda206f521d66ff6f188d063583e9484065ac
Hosa <126433144+HiImHosa23@users.noreply.github.com>
2026-05-19 09:44:39 -0600

    Getting started (WIP)
    I have created the profile page and the stylesheet. I have created a route in the navbar in which you can access the user's profile which has basic and hardcoded information of the user. I have also made sure that the other links on the navbar are aligned to the left so that it isn't in the middle of the navbar.

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,41 @@
+export default function ProfilePage() {
+    return (
+        <div>
+            <div>
+                <div>HR</div>
+                <div>
+                    <h1>Emely Rivera Guillen</h1>
+                    <p>I like movies! Movies good! I love animation!</p>
+                    <p>Joinecd May 2026</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h2>24</h2>
+                    <p>Reviews</p>
+                </div>
+                <div>
+                    <h2>100</h2>
+                    <p>Ratings</p>
+                </div>
+            </div>
+            <div>
+                <h2>Recent Reviews</h2>
+                <div>
+                    <h3>Hope</h3>
+                    <p>Rating: 10/10</p>
+                    <p>Tragic but beautifully executed</p>
+                </div>
+                <div>
+                    <h3>The Lovely Bones</h3>
+                    <p>Rating: 9/10</p>
+                    <p>Amazing movie but deserved a better ending</p>
+                </div>
+            </div>
+            <div>
+                <button>Edit Profile</button>
+                <button>Delete Account</button>
+            </div>
+        </div>
+    )
+}
